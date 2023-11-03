ff1cb208f3db8869f1de2a84b9744f664250cabb
Matthew C. Simon <127530566+acdc-digital@users.noreply.github.com>
2023-11-03 09:36:14 -0300

    Initialize Project

---
+++ main.js
@@ -0,0 +1,77 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+const { exec } = require('child_process');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false, // It's recommended to set this to false and use contextIsolation
+      contextIsolation: true,  // This should be set to true to avoid security issues
+      preload: path.join(__dirname, 'preload.js'), // If you need to preload some scripts
+      // Additional options may be required for your specific use case
+    },
+  });
+
+  const port = process.env.PORT || 3000;
+  const url = isDev
+    ? `http://localhost:${port}`
+    : `http://localhost:${port}`; // Pointing to localhost server even in production
+
+  mainWindow.loadURL(url);
+
+  // Open the DevTools in development mode.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  // Open urls in the user's browser
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if(url.startsWith('http')) {
+      shell.openExternal(url);
+      return { action: 'deny' };
+    }
+  });
+}
+
+function startServer() {
+  if (!isDev) {
+    // Start your production server here
+    const server = exec('npm run start'); // Adjust this command to your needs
+
+    server.stdout.on('data', (data) => {
+      console.log(`Server: ${data}`);
+    });
+
+    server.stderr.on('data', (data) => {
+      console.error(`Server Error: ${data}`);
+    });
+
+    server.on('close', (code) => {
+      console.log(`Server process exited with code ${code}`);
+    });
+  }
+}
+
+app.whenReady().then(() => {
+  startServer(); // Start the server
+  createWindow();
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// You would also need a 'preload.js' if you use contextIsolation: true,
+// where you can use contextBridge to expose some API to your renderer process
---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Home() {
+  const [isCodeVisible, setIsCodeVisible] = useState(false);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-cyan-500 to-blue-700 p-6 text-white">
+      <header className="absolute top-10 left-10">
+        <h1 className="text-5xl font-bold drop-shadow-md">
+          Welcome to <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-200 via-green-400 to-blue-500">Solomon Chat</span>
+        </h1>
+        <p className="mt-2 text-lg font-light">
+          Your gateway to seamless communication and collaboration.
+        </p>
+      </header>
+
+      <section className="flex flex-col items-center justify-center">
+        <Image
+          src="/next.svg" // Replace with your logo
+          alt="Solomon Chat Logo"
+          width={150}
+          height={150}
+          priority
+        />
+
+        <button
+          className="mt-10 rounded-full bg-white px-6 py-3 text-sm font-medium text-gray-800 transition duration-300 ease-in-out hover:bg-gray-100 focus:outline-none focus:ring focus:ring-blue-300"
+          onClick={() => setIsCodeVisible(!isCodeVisible)}
+        >
+          {isCodeVisible ? 'Hide' : 'Show'} Secret Code
+        </button>
+
+        {isCodeVisible && (
+          <div className="mt-5 rounded bg-black/50 p-4">
+            <code className="break-words">SC-123456789</code> {/* This could be a dynamic token */}
+          </div>
+        )}
+      </section>
+
+      <footer className="absolute bottom-10 flex w-full justify-center text-xs">
+        <span>
+          Crafted with ❤️ by the Solomon Team
+        </span>
+      </footer>
+    </main>
+  );
+}
